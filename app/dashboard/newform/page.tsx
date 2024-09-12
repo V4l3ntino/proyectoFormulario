59595722e4840737f5100b898f6f8856121dc267
Valentino <valentinohontar15@gmail.com>
2024-09-12 17:09:53 +0200

    Nuevos campos agregados en Nuevo Formulario

--- app/dashboard/newform/page.tsx
+++ app/dashboard/newform/page.tsx
@@ -18,8 +18,12 @@ const fetchUsers = async (): Promise<Person[]> => {
 const newform = () => {
     const [name, setName] = useState('');
     const refElement = useRef<HTMLInputElement>(null);
+    const experienciaRefElement = useRef<HTMLInputElement>(null);
+
     const [lista, setLista] = useState<Person[]>([])
     const [json, setJson] = useState<Person[]>([])
+    const [men, setMen] = useState<boolean>(false)
+    const [women, setWomen] = useState<boolean>(false)
 
     useEffect(() => {
         filter()
@@ -34,6 +38,7 @@ const newform = () => {
       fetchData();
     }, []);
 
+
     const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
     }
@@ -46,8 +51,9 @@ const newform = () => {
         })
         setLista(name.length > 0 ? listaFiltrada : [])
     }
-    const choose = (text:string) => {
-        refElement.current!.value = text
+    const choose = (operario:Person) => {
+        refElement.current!.value = `${operario.nombre}, ${operario.apellido} | Id: ${operario.id}`
+        experienciaRefElement.current!.value = operario.experiencia.toString()
         setLista([])
     }
     return ( 
@@ -62,15 +68,15 @@ const newform = () => {
                     <form action="" onSubmit={handleSubmit} className="w-full">
                         <div className="flex gap-2 lg:gap-5 flex-col lg:flex-row w-full">
                             <div className="lg:w-2/3 w-full">
-                                <label htmlFor="">Nombre</label>
+                                <label htmlFor="">Operario</label>
                                 <input ref={refElement} onChange={(e) => {setName(e.target.value)}} 
-                                type="text" name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Nombre " required />
+                                type="text" name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Escribe el apellido " required />
                                     
                                     {lista.length > 0 ? 
                                         <div className="bg-slate-100 flex flex-col border-[1px] border-solid border-gray-50 max-h-40 overflow-auto rounded mt-2">
                                             {
                                                 (lista).map((item, index) => (
-                                                    <span onClick={()=> {choose(item.nombre)}} className="cursor-pointer text-start mx-[2px] hover:bg-black/15 rounded px-2">{item.nombre}, {item.apellido} | Id: {item.id}</span>
+                                                    <span onClick={()=> {choose(item)}} className="cursor-pointer text-start mx-[2px] hover:bg-black/15 rounded px-2">{item.nombre}, {item.apellido} | Id: {item.id}</span>
                                                 ))
                                             }
                                         </div>
@@ -79,6 +85,21 @@ const newform = () => {
                             <div className="lg:w-1/3 w-full">
                                 <label htmlFor="">Edad</label>
                                 <input type="email" name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Edad " required />
+                            </div>
+                        </div>
+                        <div className="flex gap-2 lg:gap-5 flex-col lg:flex-row w-full">
+                            <div className="lg:w-1/6 w-full">
+                                <label htmlFor="">Experiencia en Meses</label>
+                                <input  
+                                type="number" ref={experienciaRefElement} name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Experiencia " required />
+                            </div>
+                            <div className="lg:w-1/6 w-full">
+                                <label htmlFor="">Hombre</label>
+                                <input  type="checkbox" onClick={() => {setMen(true); setWomen(false)}} checked={men}  name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Edad " required />
+                            </div>
+                            <div className="lg:w-1/6 w-full">
+                                <label htmlFor="">Mujer</label>
+                                <input  type="checkbox" onClick={() => {setWomen(true); setMen(false)}} checked={women} name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Edad " required />
                             </div>
                         </div>
                     </form>
