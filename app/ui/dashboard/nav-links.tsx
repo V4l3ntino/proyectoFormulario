f99b2f7ff99c1d029ece32bc645cf50fbc9a4e73
vhondos <vhondos126@g.educaand.es>
2024-10-03 13:10:39 +0200

    Fix error ruta seleccionada

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,7 +11,7 @@ import { usePathname } from 'next/navigation';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard/', icon: HomeIcon },
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
   { name: 'Editar Formulario', href: '/dashboard/editform', icon: PencilSquareIcon },
 ];
 
