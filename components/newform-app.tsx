9ed64db276f5921e971476055d69a39a1b6294aa
vhondos <vhondos126@g.educaand.es>
2024-10-03 12:42:56 +0200

    guarda el valor de los inputs en Nombre, Edad, Experiencia

--- components/newform-app.tsx
+++ components/newform-app.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Person } from "@/interfaces/interfaces";
 
 type Props = {
@@ -8,12 +8,12 @@ type Props = {
 }
 
 const NewformApp:React.FC<Props> = ({propJson}) => {
-    const [name, setName] = useState('');
-    const refElement = useRef<HTMLInputElement>(null);
-    const experienciaRefElement = useRef<HTMLInputElement>(null);
-
     const [lista, setLista] = useState<Person[]>([])
     const [json, setJson] = useState<Person[]>(propJson)
+
+    const [name, setName] = useState('');
+    const edadRefElement = useRef<HTMLInputElement>(null);
+    const experienciaRefElement = useRef<HTMLInputElement>(null);
     const [men, setMen] = useState<boolean>(false)
     const [women, setWomen] = useState<boolean>(false)
 
@@ -22,7 +22,23 @@ const NewformApp:React.FC<Props> = ({propJson}) => {
     },[name])
 
 
-    useEffect(() => {}, [])
+    const saveInStorage = (tipo: string, value: any) =>{
+        localStorage.setItem(tipo, JSON.stringify(value))
+    }
+
+    useEffect(() => {
+        const name = localStorage.getItem("name")
+        const edad = JSON.parse(localStorage.getItem("edad")!) || 18
+        const experiencia = JSON.parse(localStorage.getItem("experiencia")!) || ``
+
+        name ? setName(JSON.parse(name)) : ``;
+        try {
+            edad < 100 ? edadRefElement.current!.value = edad : edadRefElement.current!.value = "18"; 
+            experienciaRefElement.current!.value = experiencia
+        } catch (error) {
+            console.warn(error)
+        }
+    }, [])
 
     const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -37,8 +53,10 @@ const NewformApp:React.FC<Props> = ({propJson}) => {
         setLista(name.length > 0 ? listaFiltrada : [])
     }
     const choose = (operario:Person) => {
-        refElement.current!.value = `${operario.nombre}, ${operario.apellido} | Id: ${operario.id}`
-        experienciaRefElement.current!.value = operario.experiencia.toString()
+        setName(`${operario.nombre}, ${operario.apellido} | Id: ${operario.id}`);
+        localStorage.setItem("name", JSON.stringify(`${operario.nombre}, ${operario.apellido} | Id: ${operario.id}`))
+        experienciaRefElement.current!.value = operario.experiencia.toString();
+        localStorage.setItem("experiencia", JSON.stringify(operario.experiencia))
         setLista([])
     }
     return ( 
@@ -48,8 +66,8 @@ const NewformApp:React.FC<Props> = ({propJson}) => {
                         <div className="flex gap-2 lg:gap-5 flex-col lg:flex-row w-full">
                             <div className="lg:w-2/3 w-full">
                                 <label htmlFor="">Operario</label>
-                                <input ref={refElement} onChange={(e) => {setName(e.target.value)}} 
-                                type="text" name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Escribe el apellido " required />
+                                <input onChange={(e: ChangeEvent<HTMLInputElement>) => {setName(e.target.value); saveInStorage("name",e.target.value)}} value={name}
+                                type="text"  className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Escribe el apellido " required />
                                     
                                     {lista.length > 0 ? 
                                         <div className="bg-slate-100 flex flex-col border-[1px] border-solid border-gray-50 max-h-40 overflow-auto rounded mt-2">
@@ -63,14 +81,14 @@ const NewformApp:React.FC<Props> = ({propJson}) => {
                             </div>
                             <div className="lg:w-1/3 w-full">
                                 <label htmlFor="">Edad</label>
-                                <input type="email" name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Edad " required />
+                                <input ref={edadRefElement} onChange={(e) => {saveInStorage("edad",e.target.value)}} type="number" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Edad " required />
                             </div>
                         </div>
                         <div className="flex gap-2 lg:gap-5 flex-col lg:flex-row w-ful">
                             <div className="lg:w-1/6 w-full">
-                                <label htmlFor="">Experiencia en Meses</label>
-                                <input  
-                                type="number" ref={experienciaRefElement} name="floating_email" className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Experiencia " required />
+                                <label htmlFor="">Meses</label>
+                                <input  onChange={(e) => {saveInStorage("experiencia",e.target.value)}}
+                                type="number" ref={experienciaRefElement}  className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder=" Experiencia " required />
                             </div>
                             <div className="flex gap-2 w-2/3">
                                 <div className="lg:w-1/6 w-full">
