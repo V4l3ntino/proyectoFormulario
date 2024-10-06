7d45d5602cee21b629b71f8ee549f74cb6f2aa66
V4l3ntino <valentinohontar15@gmail.com>
2024-10-06 20:46:20 +0200

    fix errors

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Abel } from 'next/font/google'
+import { Inter, Abel, Lusitana } from 'next/font/google'
 
 export const inter = Inter({subsets: ['latin'], weight: ['900', '200']})
 export const abel = Abel({subsets: ['latin'], weight: ['400']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
